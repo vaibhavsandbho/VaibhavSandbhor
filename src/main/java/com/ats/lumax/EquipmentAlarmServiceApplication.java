7ad754e982f008f6191bdc459e5837cdc355d9c2
vaibhavsandbho <156406886+vaibhavsandbho@users.noreply.github.com>
2025-08-28 14:00:30 +0530

    add tomcat depednecy

--- src/main/java/com/ats/lumax/EquipmentAlarmServiceApplication.java
+++ src/main/java/com/ats/lumax/EquipmentAlarmServiceApplication.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -14,6 +16,22 @@ public class EquipmentAlarmServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(EquipmentAlarmServiceApplication.class, args);
+        
+        
+        
+    }
+    @Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+ 
+				
+			//	registry.addMapping("/**").allowedOrigins("http://192.168.1.100:8080");
+ 
+				registry.addMapping("/**").allowedOrigins("http://localhost:3000","http://localhost:8090");
+			}
+		};
     }
     
     @Bean
